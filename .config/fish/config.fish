c5d6c8c7d860e15ab2e00a0d91c5027a8368cead
Shreyas Chand <shreyas.chand@gmail.com>
2015-08-01 18:28:58 -0700

    Better path setting using conditionals

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,11 @@
-set -x ANDROID_HOME /users/chandsie/Library/Android/sdk
-set -x PATH {$ANDROID_HOME}/tools {$ANDROID_HOME}/platform-tools /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin $PATH
+set -x PATH /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin $PATH
+
+if test -d /users/chandsie/Library/Android/sdk
+    set -x PATH /users/chandsie/Library/Android/sdk/tools /users/chandsie/Library/Android/sdk/platform-tools $PATH
+end
+
+if test -d /usr/local/blackbox/bin
+    set -x PATH /usr/local/blackbox/bin $PATH
+end
 
 eval (python3 -m virtualfish compat_aliases auto_activation)
